e47caf2f7eb046e3a02afad64a12714ceb0ffaa1
Apoorva <19apoorva.v@gmail.com>
2019-09-04 16:06:06 +0530

    first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface City {
+  name: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'derivatives-analysis-app';
+
+  cities1: City[];
+    
+    cities2: City[];
+
+    selectedCity1: City;
+    
+    selectedCity2: City;
+
+    constructor() {
+        //SelectItem API with label-value pairs
+        this.cities1 = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+        ];
+        
+        //An array of cities
+        this.cities2 = [
+            {name: 'New York', code: 'NY'},
+            {name: 'Rome', code: 'RM'},
+            {name: 'London', code: 'LDN'},
+            {name: 'Istanbul', code: 'IST'},
+            {name: 'Paris', code: 'PRS'}
+        ];
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { RadioButtonModule } from 'primeng/primeng';
+import {DropdownModule} from 'primeng/dropdown';
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
 import { CurrentHoldingsComponentComponent } from './current-holdings-component/current-holdings-component.component';
 import { StrategyBuilderComponentComponent } from './strategy-builder-component/strategy-builder-component.component';
 import { AnalysisComponentComponent } from './analysis-component/analysis-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { AnalysisComponentComponent } from './analysis-component/analysis-compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RadioButtonModule
+    RadioButtonModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
